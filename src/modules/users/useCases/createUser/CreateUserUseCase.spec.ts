5bd364ed2524ad53220968427f63a25c2978242b
Luiz Rioja <lulyrioja@gmail.com>
2021-08-01 16:02:09 -0300

    :alien: Creating the CreateUser Tests

---
+++ src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
@@ -0,0 +1,39 @@
+import { InMemoryUsersRepository } from "@modules/users/repositories/in-memory/InMemoryUsersRepository";
+import { CreateUserError } from "./CreateUserError";
+import { CreateUserUseCase } from "./CreateUserUseCase";
+
+let createUserUseCase: CreateUserUseCase;
+let inMemoryUsersRepository: InMemoryUsersRepository;
+
+describe("Create User", () => {
+  beforeEach(() => {
+    inMemoryUsersRepository = new InMemoryUsersRepository();
+    createUserUseCase = new CreateUserUseCase(inMemoryUsersRepository);
+  });
+
+  it("should be able to create a new user", async () => {
+    const user = await inMemoryUsersRepository.create({
+      email: "user@test.com",
+      name: "User test",
+      password: "123456",
+    });
+
+    expect(user).toHaveProperty("id");
+  });
+
+  it("should not be able to create a user if there is already a user with the same email", () => {
+    expect(async () => {
+      await inMemoryUsersRepository.create({
+        email: "user@test.com",
+        name: "User test",
+        password: "123456",
+      });
+
+      await inMemoryUsersRepository.create({
+        email: "user@test.com",
+        name: "User test",
+        password: "123456",
+      });
+    }).rejects.toBeInstanceOf(CreateUserError);
+  });
+});
