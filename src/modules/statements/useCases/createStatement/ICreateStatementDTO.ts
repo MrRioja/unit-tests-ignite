14f0e7ccb6051d465604530c62ae8f9c86a51c85
Luiz Rioja <lulyrioja@gmail.com>
2021-08-08 19:58:18 -0300

    :alien: Changing CreateStatementDTO's Type

--- src/modules/statements/useCases/createStatement/ICreateStatementDTO.ts
+++ src/modules/statements/useCases/createStatement/ICreateStatementDTO.ts
@@ -1,10 +1,9 @@
-import { Statement } from "../../entities/Statement";
+interface ICreateStatementDTO {
+  user_id: string;
+  description: string;
+  amount: number;
+  type: string;
+  sender_id?: string;
+}
 
-export type ICreateStatementDTO =
-Pick<
-  Statement,
-  'user_id' |
-  'description' |
-  'amount' |
-  'type'
->
+export { ICreateStatementDTO };
