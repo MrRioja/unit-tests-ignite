dc9f9803eef5ee2bd9c67a12dff1121ed434e11c
Luiz Rioja <lulyrioja@gmail.com>
2021-08-08 21:37:16 -0300

    :alien: Fixing the transfer route controller

--- src/routes/statements.routes.ts
+++ src/routes/statements.routes.ts
@@ -1,13 +1,15 @@
 import { Router } from "express";
 
-import { CreateStatementController } from "../modules/statements/useCases/createStatement/CreateStatementController";
-import { GetBalanceController } from "../modules/statements/useCases/getBalance/GetBalanceController";
-import { GetStatementOperationController } from "../modules/statements/useCases/getStatementOperation/GetStatementOperationController";
 import { ensureAuthenticated } from "../shared/infra/http/middlwares/ensureAuthenticated";
+import { GetBalanceController } from "../modules/statements/useCases/getBalance/GetBalanceController";
+import { CreateTransferController } from "../modules/statements/useCases/createTransfer/CreateTransferController";
+import { CreateStatementController } from "../modules/statements/useCases/createStatement/CreateStatementController";
+import { GetStatementOperationController } from "../modules/statements/useCases/getStatementOperation/GetStatementOperationController";
 
 const statementRouter = Router();
 const getBalanceController = new GetBalanceController();
 const createStatementController = new CreateStatementController();
+const createTransferController = new CreateTransferController();
 const getStatementOperationController = new GetStatementOperationController();
 
 statementRouter.use(ensureAuthenticated);
@@ -15,7 +17,7 @@ statementRouter.use(ensureAuthenticated);
 statementRouter.get("/balance", getBalanceController.execute);
 statementRouter.post("/deposit", createStatementController.execute);
 statementRouter.post("/withdraw", createStatementController.execute);
-statementRouter.post("/transfers/:user_id", createStatementController.execute);
+statementRouter.post("/transfers/:user_id", createTransferController.handle);
 statementRouter.get("/:statement_id", getStatementOperationController.execute);
 
 export { statementRouter };
